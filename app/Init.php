e336a31450f49cd23b7a6bcc3307c8b3c5ac5b4a
romarioL <estreldamanha585@gmail.com>
2019-12-31 13:42:27 -0300

    Sniffer

---
+++ app/Init.php
@@ -0,0 +1,25 @@
+<?php 
+
+namespace App;
+
+class Init 
+{
+
+	private $pet;
+	private $dono; 
+	private $distancia; 
+
+
+	public function __construct(IPEt $pet, IDono $dono, IConfiguradorDistancia $distancia)
+	{
+       $this->pet = $pet;
+       $this->dono = $dono;
+       $this->distancia = $distancia;
+	}
+
+	public function display()
+	{
+		$monitoria = new Monitoria($this->pet, $this->dono, $this->distancia);
+        return $monitoria->monitorarPetlonge();
+	}
+}
--- index.php
+++ index.php
@@ -9,6 +9,7 @@ use App\Pet;
 use App\DistanciaPetDono;
 use App\EstaLonge;
 use App\Monitoria;
+use App\Init;
 
 
 
@@ -17,8 +18,12 @@ $dono = new Dono("Romis",-05.14, -38.09 );
 
 $pet = new Pet("Bydu", -04.93, -37.97 );
 
-$monitoria = new Monitoria($pet, $dono, $distancia);
- echo $monitoria->monitorarPetlonge();
+$init = new Init($pet, $dono, $distancia);
+
+ echo $init->display();
+
+
+
 
 
  
