82539da584fc5c512effa35d6a7e300766bbcd6f
Romis Sousa <estreladamanha585@gmail.com>
2020-07-27 10:25:04 -0300

    Retirar comment

--- app/geometry/CriarGeoJson.php
+++ app/geometry/CriarGeoJson.php
@@ -1,17 +1,5 @@
 <?php
 
-/*
-
-{ "type": "Feature",
-         "geometry": {
-           "type": "Polygon",
-           "coordinates": [
-             [ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0],
-               [100.0, 1.0], [100.0, 0.0] ]
-             ]
-         }
-*/
-
 namespace App\geometry;
 
 use App\geometry\InitGeoJson;
@@ -19,12 +7,11 @@ use App\geometry\InitGeoJson;
 class CriarGeoJson
 {
     public function criarGeoJsonString(
-       String $type,
-       String $geometry,
-       InitGeoJson $init,
-       String $place = "Some place"
-      ): String
-    {
+        String $type,
+        String $geometry,
+        InitGeoJson $init,
+        String $place = "Some place"
+    ): String {
         $geoJsonArray = [
             "type" =>  $type,
             "geometry" => [
